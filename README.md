d5ddf663f5e3d0a832ee81cf19bad040323f3985
yashkumar22012009 <74101335+yashkumar22012009@users.noreply.github.com>
2021-01-20 17:39:57 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# werecking-ball
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
---
+++ ball.js
@@ -0,0 +1,24 @@
+class Ball {
+    constructor(x,y,width,height,angle) {
+      var options = {
+          density:1,
+          frictionAir:0.005
+          
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle=this.body.angle
+      push()
+      translate (pos.x,pos.y);
+      rotate(angle)
+      rectMode(CENTER);
+      fill("brown");
+      ellipse(0, 0, this.width, this.height);
+      pop()
+    }
+  };
